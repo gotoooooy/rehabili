a320deb5ddd55f195d9d2a90c76f331f9de5c843
gotoooooy <gotoooooy@gmail.com>
2015-05-18 03:45:47 +0900

    20150518

---
+++ nw_simulator/src/nw_simulator/Server.java
@@ -0,0 +1,16 @@
+package nw_simulator;
+
+//サーバー
+public class Server extends Node {
+	Server() {
+		super();
+	}
+	
+	Server(int no) {
+		super(no);
+	}
+	
+	Server(int no, int portNum) {
+		super(no, portNum);
+	}
+}
