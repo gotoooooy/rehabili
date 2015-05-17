6db8b238045e1c55bf7017f12300e1140c5069d4
gotoooooy <gotoooooy@gmail.com>
2015-05-18 01:47:14 +0900

    20150518

--- nw_simulator/src/nw_simulator/main.java
+++ nw_simulator/src/nw_simulator/main.java
@@ -2,6 +2,17 @@ package nw_simulator;
 
 public class main {
 	public static void main (String args[]) {
-		System.out.println("Hello world!");		
+		System.out.println("リバビリマシーン");
+		
+		Link l1 = new Link();
+		Link l2 = new Link();
+		Link l3 = new Link();
+		PC p = new PC(0, 3);
+		
+		p.connectLink(0, l1);
+		p.connectLink(1, l2);
+		p.connectLink(2, l3);
+		System.out.println(p.getDuplex(1));
+		System.out.println(p.getBandwidth(1));
 	}
 }
